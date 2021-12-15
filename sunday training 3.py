1f41ac0df4cfd6fff6517bf7baaf6f242a64a0c0
Ibrahim-hub855 <96174698+Ibrahim-hub855@users.noreply.github.com>
2021-12-15 12:31:11 +0100

    Add files via upload
    New file uploaded in the child branch .

---
+++ sunday training 3.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  5 15:40:06 2020
+
+@author: IBCIRCUIT
+"""
+
+
+print("Only stop argument value specified in range function")
+for i in range(100):
+    print(f"{i}")
+print ("Both start and stop argument values specified in range function")
+for i in range (2, 100):
+    print(f"{i}")
+print ("All the three arguments values(Start, Stop, Step) specified in range function")
+for i in range (1, 100, 2):
+    print(f"{i}")
