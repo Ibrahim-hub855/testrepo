3118eaf60391b3e8894b207d4f31606344854a9d
Ibrahim-hub855 <96174698+Ibrahim-hub855@users.noreply.github.com>
2021-12-15 11:48:34 +0100

    Add files via upload

---
+++ history.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Sat Jul  4 10:24:15 2020)---
+runfile('C:/Users/IBCIRCUIT/.spyder-py3/temp.py', wdir='C:/Users/IBCIRCUIT/.spyder-py3')
+runfile('C:/Users/IBCIRCUIT/Desktop/JES/temp.py', wdir='C:/Users/IBCIRCUIT/Desktop/JES')
+
+## ---(Sun Jul  5 12:41:20 2020)---
+runfile('C:/Users/IBCIRCUIT/.spyder-py3/temp.py', wdir='C:/Users/IBCIRCUIT/.spyder-py3')
+runfile('C:/Users/IBCIRCUIT/.spyder-py3/untitled0.py', wdir='C:/Users/IBCIRCUIT/.spyder-py3')
+runfile('C:/Users/IBCIRCUIT/.spyder-py3/untitled1.py', wdir='C:/Users/IBCIRCUIT/.spyder-py3')
+runfile('C:/Users/IBCIRCUIT/.spyder-py3/untitled2.py', wdir='C:/Users/IBCIRCUIT/.spyder-py3')
+runfile('C:/Users/IBCIRCUIT/.spyder-py3/untitled3.py', wdir='C:/Users/IBCIRCUIT/.spyder-py3')
+runfile('C:/Users/IBCIRCUIT/.spyder-py3/untitled4.py', wdir='C:/Users/IBCIRCUIT/.spyder-py3')
+runfile('C:/Users/IBCIRCUIT/.spyder-py3/untitled5.py', wdir='C:/Users/IBCIRCUIT/.spyder-py3')
+
+## ---(Fri Jul 10 08:44:46 2020)---
+runfile('C:/Users/IBCIRCUIT/.spyder-py3/sunday training 6.py', wdir='C:/Users/IBCIRCUIT/.spyder-py3')
+
+## ---(Mon Jul 13 14:17:17 2020)---
+runfile('C:/Users/IBCIRCUIT/.spyder-py3/untitled0.py', wdir='C:/Users/IBCIRCUIT/.spyder-py3')
+
+## ---(Wed Jul 22 11:25:00 2020)---
+runfile('C:/Users/IBCIRCUIT/.spyder-py3/untitled0.py', wdir='C:/Users/IBCIRCUIT/.spyder-py3')
+
+## ---(Sat Aug  1 12:12:48 2020)---
+runfile('C:/Users/IBCIRCUIT/.spyder-py3/Rescaling Data.py', wdir='C:/Users/IBCIRCUIT/.spyder-py3')
+%clear
+runfile('C:/Users/IBCIRCUIT/.spyder-py3/Rescaling Data.py', wdir='C:/Users/IBCIRCUIT/.spyder-py3')
+%clear
+runfile('C:/Users/IBCIRCUIT/.spyder-py3/Rescaling Data.py', wdir='C:/Users/IBCIRCUIT/.spyder-py3')
+%clear
+runfile('C:/Users/IBCIRCUIT/.spyder-py3/Rescaling Data.py', wdir='C:/Users/IBCIRCUIT/.spyder-py3')
